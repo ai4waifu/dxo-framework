1640cbff986209886ecaebf867be90d238fa9dac
aster <aster@vers.site>
2009-10-24 22:21:25 +0000

    ♻️ Route CUDA GEMM through `titan-runtime` facade

--- projects/compilers/dxo-core/src/cuda.rs
+++ projects/compilers/dxo-core/src/cuda.rs
@@ -1,95 +1,65 @@
-//! Titan CUDA facade for contiguous f32 GEMM.
+//! CUDA GEMM adapter — dispatches through `titan-runtime` only.
 //!
-//! Compiles without the CUDA Toolkit (Driver API via `libloading`). Runtime
-//! availability depends on a loaded NVIDIA driver and at least one device.
+//! DXO must not compile kernels, cache artifacts, or orchestrate upload/launch/readback.
+//! Device session bootstrap still uses `CudaDriver::open` until Titan exposes
+//! `Runtime::open_cuda_primary()` (see handoff in workspace design docs).
 
-use std::sync::{Arc, Mutex, OnceLock};
+use std::sync::{Mutex, OnceLock};
 
-use titan_backend_cuda::{CudaCompiler, CudaDriver, GemmF32Descriptor, gemm_f32_abi};
-use titan_hal::{BackendDriver, DeviceSession, LaunchGeometry};
-use titan_kernel::{BasicBlock, BlockId, KernelAbi, KernelArg, KernelModule};
-use titan_types::{BackendId, DType, DeviceId, KernelId};
+use titan_backend_cuda::CudaDriver;
+use titan_graph::{EffectContract, OpRequest, TensorSpec};
+use titan_hal::BackendDriver;
+use titan_runtime::Runtime;
+use titan_tensor::{Device, Tensor};
+use titan_types::{AliasContract, AttrMap, DType, Layout, MemoryEffect, OperatorId, Shape, SourceSpan, Strides};
 
 use crate::tensor::TensorError;
 
-static CUDA_SESSION: OnceLock<Result<Arc<dyn DeviceSession>, String>> = OnceLock::new();
-static GEMM_CACHE: OnceLock<Mutex<Option<CachedGemm>>> = OnceLock::new();
-
-struct CachedGemm {
-    fingerprint_key: String,
-    bytes: Vec<u8>,
-    abi: KernelAbi,
-    metadata: titan_types::KernelLaunchMetadata,
+struct CudaState {
+    runtime: Mutex<Runtime>,
+    device: Device,
 }
 
-fn gemm_ir(abi: KernelAbi) -> KernelModule {
-    KernelModule {
-        kernel_id: KernelId("gemm.f32".into()),
-        entry: BlockId(0),
-        blocks: vec![BasicBlock { id: BlockId(0), params: vec![], instructions: vec![] }],
-        abi,
+static CUDA_STATE: OnceLock<Result<CudaState, String>> = OnceLock::new();
+
+fn dxo_source() -> SourceSpan {
+    SourceSpan { file: "dxo-core".into(), line: 0, column: 0 }
+}
+
+fn gemm_request(lhs: titan_tensor::TensorHandle, rhs: titan_tensor::TensorHandle, output_shape: [u64; 2]) -> OpRequest {
+    OpRequest {
+        operator: OperatorId("gemm".into()),
+        inputs: vec![lhs, rhs],
+        outputs: vec![TensorSpec {
+            dtype: DType::F32,
+            strides: Strides(vec![output_shape[1] as i64, 1]),
+            shape: Shape(output_shape.to_vec()),
+            layout: Layout::Contiguous,
+            alias: AliasContract::NoAlias,
+        }],
+        attrs: AttrMap::new(),
+        effects: EffectContract { memory: MemoryEffect::Writes, deterministic: true },
+        source: dxo_source(),
     }
+}
+
+fn cuda_state() -> Result<&'static CudaState, TensorError> {
+    let init = CUDA_STATE.get_or_init(|| {
+        let driver = CudaDriver::open().map_err(|e| e.to_string())?;
+        let devices = driver.enumerate().map_err(|e| e.to_string())?;
+        let fingerprint = devices.first().ok_or_else(|| "no CUDA devices enumerated".to_string())?;
+        let session = driver.open(fingerprint.device).map_err(|e| e.to_string())?;
+        let device = Device::from_session(session);
+        let cache_path = std::env::temp_dir().join("dxo-titan-runtime-cache");
+        let runtime = Runtime::open(cache_path);
+        Ok(CudaState { runtime: Mutex::new(runtime), device })
+    });
+    init.as_ref().map_err(|msg| TensorError::Device(format!("CUDA unavailable: {msg}")))
 }
 
 /// Whether a Titan CUDA session can be opened on this machine.
 pub fn is_available() -> bool {
-    cuda_session().is_ok()
-}
-
-fn cuda_session() -> Result<Arc<dyn DeviceSession>, TensorError> {
-    let cached = CUDA_SESSION.get_or_init(|| {
-        let driver = CudaDriver::open().map_err(|e| e.to_string())?;
-        let devices = driver.enumerate().map_err(|e| e.to_string())?;
-        if devices.is_empty() {
-            return Err("no CUDA devices enumerated".into());
-        }
-        driver.open(DeviceId { backend: BackendId::Cuda, ordinal: 0 }).map_err(|e| e.to_string())
-    });
-    match cached {
-        Ok(s) => Ok(s.clone()),
-        Err(msg) => Err(TensorError::Device(format!("CUDA unavailable: {msg}"))),
-    }
-}
-
-fn cached_gemm(session: &Arc<dyn DeviceSession>) -> Result<CachedGemm, TensorError> {
-    let fp = session.fingerprint();
-    let key = format!("{:?}:{}:{}", fp.device.backend, fp.device.ordinal, fp.capability_revision);
-    let slot = GEMM_CACHE.get_or_init(|| Mutex::new(None));
-    let mut guard = slot.lock().map_err(|_| TensorError::Device("CUDA gemm cache poisoned".into()))?;
-    if let Some(hit) = guard.as_ref() {
-        if hit.fingerprint_key == key {
-            return Ok(CachedGemm {
-                fingerprint_key: hit.fingerprint_key.clone(),
-                bytes: hit.bytes.clone(),
-                abi: hit.abi.clone(),
-                metadata: hit.metadata.clone(),
-            });
-        }
-    }
-    let abi = gemm_f32_abi();
-    let artifact = CudaCompiler
-        .compile_artifact(&gemm_ir(abi.clone()), &abi, fp)
-        .map_err(|e| TensorError::Device(format!("CUDA gemm compile: {e}")))?;
-    let cached =
-        CachedGemm { fingerprint_key: key, bytes: artifact.ptx().to_vec(), abi, metadata: artifact.metadata().clone() };
-    *guard = Some(CachedGemm {
-        fingerprint_key: cached.fingerprint_key.clone(),
-        bytes: cached.bytes.clone(),
-        abi: cached.abi.clone(),
-        metadata: cached.metadata.clone(),
-    });
-    Ok(cached)
-}
-
-fn f32_bytes(data: &[f32]) -> Vec<u8> {
-    data.iter().flat_map(|v| v.to_le_bytes()).collect()
-}
-
-fn f32_from_bytes(bytes: &[u8]) -> Result<Vec<f32>, TensorError> {
-    if bytes.len() % 4 != 0 {
-        return Err(TensorError::Device("CUDA download length not multiple of 4".into()));
-    }
-    Ok(bytes.chunks_exact(4).map(|c| f32::from_le_bytes([c[0], c[1], c[2], c[3]])).collect())
+    cuda_state().is_ok()
 }
 
 /// Contiguous row-major f32 GEMM on CUDA: `A[m,k] @ B[k,n] -> C[m,n]`.
@@ -107,80 +77,24 @@ pub fn gemm_f32(lhs: &[f32], m: usize, k: usize, rhs: &[f32], n: usize) -> Resul
         )));
     }
 
-    let session = cuda_session()?;
-    let m_u = u32::try_from(m).map_err(|_| TensorError::Device("M exceeds u32".into()))?;
-    let k_u = u32::try_from(k).map_err(|_| TensorError::Device("K exceeds u32".into()))?;
-    let n_u = u32::try_from(n).map_err(|_| TensorError::Device("N exceeds u32".into()))?;
-
-    GemmF32Descriptor {
-        m: m_u,
-        n: n_u,
-        k: k_u,
-        lhs_shape: [m_u, k_u],
-        rhs_shape: [k_u, n_u],
-        output_shape: [m_u, n_u],
-        lhs_dtype: DType::F32,
-        rhs_dtype: DType::F32,
-        output_dtype: DType::F32,
-        lhs_contiguous: true,
-        rhs_contiguous: true,
-        output_contiguous: true,
-        transpose_lhs: false,
-        transpose_rhs: false,
-    }
-    .validate()
-    .map_err(|e| TensorError::Device(format!("CUDA gemm contract: {e}")))?;
-
-    let lhs_bytes = f32_bytes(lhs);
-    let rhs_bytes = f32_bytes(rhs);
-    let out_bytes_len = m * n * 4;
-
-    let lhs_buf = session.allocate(lhs_bytes.len(), 4).map_err(|e| TensorError::Device(format!("CUDA allocate lhs: {e}")))?;
-    let rhs_buf = session.allocate(rhs_bytes.len(), 4).map_err(|e| TensorError::Device(format!("CUDA allocate rhs: {e}")))?;
-    let out_buf = session.allocate(out_bytes_len, 4).map_err(|e| TensorError::Device(format!("CUDA allocate out: {e}")))?;
-
-    let stream = session.create_stream().map_err(|e| TensorError::Device(format!("CUDA stream: {e}")))?;
-    let up_l = session
-        .upload(stream.as_ref(), lhs_buf.as_ref(), &lhs_bytes)
+    let state = cuda_state()?;
+    let cuda_lhs = Tensor::<f32, 2>::from_slice(&state.device, [m, k], lhs)
         .map_err(|e| TensorError::Device(format!("CUDA upload lhs: {e}")))?;
-    session.wait(up_l.as_ref()).map_err(|e| TensorError::Device(format!("CUDA wait lhs: {e}")))?;
-    let up_r = session
-        .upload(stream.as_ref(), rhs_buf.as_ref(), &rhs_bytes)
+    let cuda_rhs = Tensor::<f32, 2>::from_slice(&state.device, [k, n], rhs)
         .map_err(|e| TensorError::Device(format!("CUDA upload rhs: {e}")))?;
-    session.wait(up_r.as_ref()).map_err(|e| TensorError::Device(format!("CUDA wait rhs: {e}")))?;
 
-    let compiled = cached_gemm(&session)?;
-    let args = compiled
-        .abi
-        .encode(&[
-            KernelArg::Buffer { slot: 0, dtype: DType::F32, writable: false, alignment: 4, buffer: lhs_buf.clone() },
-            KernelArg::Buffer { slot: 1, dtype: DType::F32, writable: false, alignment: 4, buffer: rhs_buf.clone() },
-            KernelArg::Buffer { slot: 2, dtype: DType::F32, writable: true, alignment: 4, buffer: out_buf.clone() },
-            KernelArg::Scalar { dtype: DType::I32, bytes: (m_u as i32).to_le_bytes().to_vec() },
-            KernelArg::Scalar { dtype: DType::I32, bytes: (n_u as i32).to_le_bytes().to_vec() },
-            KernelArg::Scalar { dtype: DType::I32, bytes: (k_u as i32).to_le_bytes().to_vec() },
-        ])
-        .map_err(|e| TensorError::Device(format!("CUDA abi encode: {e}")))?;
+    let mut runtime = state.runtime.lock().map_err(|_| TensorError::Device("CUDA runtime lock poisoned".into()))?;
 
-    let kernel = session
-        .load(&compiled.bytes, &compiled.abi.abi_hash(), compiled.metadata.clone())
-        .map_err(|e| TensorError::Device(format!("CUDA load: {e}")))?;
-    let block = compiled.metadata.block[0].max(1);
-    let geometry = LaunchGeometry {
-        grid: [((m * n) as u32).div_ceil(block), 1, 1],
-        block: compiled.metadata.block,
-        shared_bytes: compiled.metadata.shared_bytes,
-    };
-    let event = session
-        .launch(stream.as_ref(), kernel.as_ref(), &args, &geometry)
-        .map_err(|e| TensorError::Device(format!("CUDA launch: {e}")))?;
-    session.wait(event.as_ref()).map_err(|e| TensorError::Device(format!("CUDA wait launch: {e}")))?;
+    let handle = runtime
+        .execute(gemm_request(cuda_lhs.handle(), cuda_rhs.handle(), [m as u64, n as u64]))
+        .map_err(|e| TensorError::Device(format!("CUDA gemm dispatch: {e}")))?
+        .wait()
+        .map_err(|e| TensorError::Device(format!("CUDA gemm completion: {e}")))?;
 
-    let mut out_bytes = vec![0u8; out_bytes_len];
-    let down = session
-        .download(stream.as_ref(), out_buf.as_ref(), &mut out_bytes)
-        .map_err(|e| TensorError::Device(format!("CUDA download: {e}")))?;
-    session.wait(down.as_ref()).map_err(|e| TensorError::Device(format!("CUDA wait download: {e}")))?;
-
-    f32_from_bytes(&out_bytes)
+    handle
+        .outputs
+        .first()
+        .ok_or_else(|| TensorError::Device("CUDA gemm returned no outputs".into()))?
+        .to_vec_f32()
+        .map_err(|e| TensorError::Device(format!("CUDA gemm readback: {e}")))
 }
