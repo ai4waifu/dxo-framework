c3e41caedefd075e97b870608eb7743505e82b84
aster <aster@vers.site>
2009-12-03 02:14:34 +0000

    🚧 Scaffold private `@dxo/diffuser` workspace package
    Reserve the diffusion pipeline product name outside placeholder and publish-npm lists.

--- scripts/ci/publish-npm.mjs
+++ scripts/ci/publish-npm.mjs
@@ -39,7 +39,7 @@ const JS_PACKAGES = [
     { dir: 'projects/runtimes/dxo-inspect', publishName: '@dxo/inspect' },
     { dir: 'projects/runtimes/dxo-graph', publishName: '@dxo/graph' },
     { dir: 'projects/runtimes/dxo-studio', publishName: '@dxo/studio' },
-    // Workspace-only stubs (hub/vision/llm/ui) are not published here.
+    // Workspace-only stubs (hub/vision/llm/diffuser/ui) are not published here.
 ];
 
 function fail(msg) {
