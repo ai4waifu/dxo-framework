b4816f6adfefb9ef11545f23a21fa01f488e3372
aster <aster@vers.site>
2009-12-08 07:30:01 +0000

    🔧 Add verify suite registry and fold Wave verifies into CI

--- scripts/build/homepage-cdn.mjs
+++ scripts/build/homepage-cdn.mjs
@@ -1,6 +1,11 @@
 /**
  * Stage VMZ static-cdn deploy root into projects/homepage/dist/cdn.
  * Cloudflare Pages output directory: projects/homepage/dist/cdn
+ *
+ * Workaround for `@vmz/vmz@0.1.12`: hashed `entry-client` / `entry-event` still
+ * use `./vmz-dom.js` relative to `/assets/`, which 404s. Mirror upstream Fix A
+ * (`rewriteJsEntryRelativeImports`) until a published VMZ release includes it.
+ * Also copy logical client modules + `lib/` so dynamic entry imports resolve.
  */
 import fs from 'node:fs';
 import path from 'node:path';
@@ -9,6 +14,8 @@ import { fileURLToPath } from 'node:url';
 const ROOT = path.resolve(path.dirname(fileURLToPath(import.meta.url)), '../..');
 const distDir = path.join(ROOT, 'projects/homepage/dist');
 const cdnDir = path.join(distDir, 'cdn');
+
+const JS_ENTRY_LOGICAL = new Set(['entry-client.js', 'entry-event.js']);
 
 function fail(msg) {
     console.error(`homepage-cdn: ${msg}`);
@@ -29,6 +36,61 @@ function copyRel(rel) {
     fs.copyFileSync(from, to);
 }
 
+function copyTree(relDir) {
+    const fromRoot = path.join(distDir, relDir);
+    if (!fs.existsSync(fromRoot)) return 0;
+    let n = 0;
+    const walk = (dir, relBase) => {
+        for (const ent of fs.readdirSync(dir, { withFileTypes: true })) {
+            const rel = path.posix.join(relBase, ent.name);
+            const from = path.join(dir, ent.name);
+            if (ent.isDirectory()) {
+                walk(from, rel);
+                continue;
+            }
+            const to = path.join(cdnDir, rel);
+            fs.mkdirSync(path.dirname(to), { recursive: true });
+            fs.copyFileSync(from, to);
+            n += 1;
+        }
+    };
+    walk(fromRoot, relDir.replace(/\\/g, '/'));
+    return n;
+}
+
+/**
+ * Same contract as VMZ `rewriteJsEntryRelativeImports` (unreleased on npm 0.1.12).
+ * @param {string} jsText
+ * @param {Record<string, string>} rewrites logical → `assets/<hash>.ext`
+ */
+function rewriteJsEntryRelativeImports(jsText, rewrites = {}) {
+    let out = String(jsText || '');
+    out = out.replace(/import\(\s*"\.\/"\s*\+/g, 'import("../"+');
+    out = out.replace(/import\(\s*'\.\/'\s*\+/g, "import('../'+");
+
+    const rewriteSpec = (spec) => {
+        const logical = String(spec || '').replace(/^\.\//, '');
+        if (!logical || logical.startsWith('../') || logical.startsWith('/')) return spec;
+        const pathPart = logical.split('?')[0];
+        const hashed = rewrites[pathPart] || rewrites[`/${pathPart}`] || rewrites[`assets/${pathPart}`];
+        if (hashed) {
+            const rel = String(hashed).replace(/^\//, '');
+            return rel.startsWith('assets/') ? `./${path.basename(rel)}` : `./${rel}`;
+        }
+        return `../${logical}`;
+    };
+
+    out = out.replace(
+        /\b((?:import|export)\s+[^'"\n]*?\s+from\s+|import\s*\(\s*)(['"])(\.\/[^'"]+)\2/g,
+        (_m, prefix, quote, spec) => `${prefix}${quote}${rewriteSpec(spec)}${quote}`,
+    );
+    out = out.replace(
+        /(^|[;\s])(import\s*)(['"])(\.\/[^'"]+)\3/gm,
+        (_m, lead, kw, quote, spec) => `${lead}${kw}${quote}${rewriteSpec(spec)}${quote}`,
+    );
+    return out;
+}
+
 if (!fs.existsSync(distDir)) {
     fail(`missing ${distDir}`);
 }
@@ -43,6 +105,8 @@ const assetsManifest = readJson('_vmz/content-addressed-assets.json');
 
 /** @type {Set<string>} */
 const relPaths = new Set();
+/** @type {Record<string, string>} */
+const rewrites = {};
 
 for (const route of staticManifest.routes ?? []) {
     if (route.htmlPath) relPaths.add(String(route.htmlPath));
@@ -58,10 +122,37 @@ if (seo.sitemap) relPaths.add(String(seo.sitemap));
 
 for (const obj of assetsManifest.objects ?? []) {
     if (obj.assetPath) relPaths.add(String(obj.assetPath));
+    if (obj.logicalPath) {
+        const logical = String(obj.logicalPath).replace(/^\//, '');
+        // Dynamic entry imports resolve against site root (`../` + id + `.client.js`).
+        relPaths.add(logical);
+        if (obj.assetPath) {
+            rewrites[logical] = String(obj.assetPath).replace(/^\//, '');
+            rewrites[`/${logical}`] = rewrites[logical];
+        }
+    }
 }
 
 for (const rel of relPaths) {
     copyRel(rel);
+}
+
+const libCopied = copyTree('lib');
+
+/** Rewrite hashed entry shells in place under cdn/assets. */
+let entryRewrites = 0;
+for (const obj of assetsManifest.objects ?? []) {
+    const logical = String(obj.logicalPath || '').replace(/^\//, '');
+    if (!JS_ENTRY_LOGICAL.has(logical) || !obj.assetPath) continue;
+    const assetRel = String(obj.assetPath).replace(/^\//, '');
+    const abs = path.join(cdnDir, assetRel);
+    if (!fs.existsSync(abs)) fail(`missing entry asset ${assetRel}`);
+    const before = fs.readFileSync(abs, 'utf8');
+    const after = rewriteJsEntryRelativeImports(before, rewrites);
+    if (after !== before) {
+        fs.writeFileSync(abs, after);
+        entryRewrites += 1;
+    }
 }
 
 /** Browser playground fetches `/dxo_lite_bg.wasm` from site root. */
@@ -76,4 +167,6 @@ if (wasmSrc) {
     console.warn(`homepage-cdn: missing ${wasmName} (run stage-homepage-wasm / build:lite-wasm)`);
 }
 
-console.log(`homepage-cdn: staged ${relPaths.size} path(s) → ${cdnDir}`);
+console.log(
+    `homepage-cdn: staged ${relPaths.size} path(s) + lib=${libCopied} entryRewrites=${entryRewrites} → ${cdnDir}`,
+);
---
+++ scripts/verify/suites.ts
@@ -0,0 +1,346 @@
+/**
+ * Single source of truth for verify suites.
+ * `pnpm verify`, `pnpm test:verify`, ordinary CI, and GPU artifact packing all read this registry.
+ */
+
+export type SuiteGroup =
+    | 'cpu'
+    | 'contract'
+    | 'product'
+    | 'gpu-smoke'
+    | 'gpu-parity'
+    | 'gpu-residency'
+    | 'gpu-model'
+    | 'gpu-precision';
+
+export type SuitePlatform = 'linux' | 'darwin' | 'win32' | 'all';
+
+export type SuiteBackend = 'cpu' | 'cuda' | 'metal' | 'webgpu' | 'none';
+
+export type SuiteDef = {
+    id: string;
+    /** Path relative to repo root. */
+    script: string;
+    group: SuiteGroup;
+    packages: string[];
+    platforms: SuitePlatform[];
+    backend: SuiteBackend[];
+    requiresGpu: boolean;
+    requiresNetwork: boolean;
+    allowSkip: boolean;
+    timeoutMs: number;
+    /** Pack into Modal GPU artifact even when not requiresGpu (e.g. CPU probe also run under CUDA). */
+    includeInGpuArtifact?: boolean;
+};
+
+export const SUITES: SuiteDef[] = [
+    {
+        id: 'smoke',
+        script: 'scripts/test/smoke.ts',
+        group: 'cpu',
+        packages: ['@dxo/core'],
+        platforms: ['all'],
+        backend: ['cpu'],
+        requiresGpu: false,
+        requiresNetwork: false,
+        allowSkip: false,
+        timeoutMs: 60_000,
+    },
+    {
+        id: 'tensor-cpu',
+        script: 'scripts/test/tensor-cpu.ts',
+        group: 'cpu',
+        packages: ['@dxo/core'],
+        platforms: ['all'],
+        backend: ['cpu'],
+        requiresGpu: false,
+        requiresNetwork: false,
+        allowSkip: false,
+        timeoutMs: 60_000,
+    },
+    {
+        id: 'autograd-fd',
+        script: 'scripts/test/autograd-fd.ts',
+        group: 'cpu',
+        packages: ['@dxo/core'],
+        platforms: ['all'],
+        backend: ['cpu'],
+        requiresGpu: false,
+        requiresNetwork: false,
+        allowSkip: false,
+        timeoutMs: 120_000,
+    },
+    {
+        id: 'nn-forward',
+        script: 'scripts/test/nn-forward.ts',
+        group: 'cpu',
+        packages: ['@dxo/core', '@dxo/nn'],
+        platforms: ['all'],
+        backend: ['cpu'],
+        requiresGpu: false,
+        requiresNetwork: false,
+        allowSkip: false,
+        timeoutMs: 60_000,
+    },
+    {
+        id: 'mnist-linear',
+        script: 'scripts/test/mnist-linear.ts',
+        group: 'cpu',
+        packages: ['@dxo/core', '@dxo/nn', '@dxo/optimizer'],
+        platforms: ['all'],
+        backend: ['cpu'],
+        requiresGpu: false,
+        requiresNetwork: false,
+        allowSkip: false,
+        timeoutMs: 120_000,
+    },
+    {
+        id: 'data-iter',
+        script: 'scripts/test/data-iter.ts',
+        group: 'cpu',
+        packages: ['@dxo/data'],
+        platforms: ['all'],
+        backend: ['none'],
+        requiresGpu: false,
+        requiresNetwork: false,
+        allowSkip: false,
+        timeoutMs: 60_000,
+    },
+    {
+        id: 'serialize-roundtrip',
+        script: 'scripts/test/serialize-roundtrip.ts',
+        group: 'cpu',
+        packages: ['@dxo/serialize', '@dxo/nn'],
+        platforms: ['all'],
+        backend: ['cpu'],
+        requiresGpu: false,
+        requiresNetwork: false,
+        allowSkip: false,
+        timeoutMs: 60_000,
+    },
+    {
+        id: 'trainer-loop',
+        script: 'scripts/test/trainer-loop.ts',
+        group: 'cpu',
+        packages: ['@dxo/train', '@dxo/nn', '@dxo/optimizer', '@dxo/data', '@dxo/serialize'],
+        platforms: ['all'],
+        backend: ['cpu'],
+        requiresGpu: false,
+        requiresNetwork: false,
+        allowSkip: false,
+        timeoutMs: 120_000,
+    },
+    {
+        id: 'framework-core-cnn',
+        script: 'scripts/test/framework-core-cnn.ts',
+        group: 'cpu',
+        packages: ['@dxo/core', '@dxo/nn', '@dxo/serialize'],
+        platforms: ['all'],
+        backend: ['cpu'],
+        requiresGpu: false,
+        requiresNetwork: false,
+        allowSkip: false,
+        timeoutMs: 180_000,
+    },
+    {
+        id: 'framework-core-transformer',
+        script: 'scripts/test/framework-core-transformer.ts',
+        group: 'cpu',
+        packages: ['@dxo/core', '@dxo/nn', '@dxo/serialize'],
+        platforms: ['all'],
+        backend: ['cpu'],
+        requiresGpu: false,
+        requiresNetwork: false,
+        allowSkip: false,
+        timeoutMs: 180_000,
+    },
+    {
+        id: 'g3-contract',
+        script: 'scripts/test/g3-contract.ts',
+        group: 'contract',
+        packages: ['@dxo/core', '@dxo/nn', '@dxo/optimizer'],
+        platforms: ['all'],
+        backend: ['cpu'],
+        requiresGpu: false,
+        requiresNetwork: false,
+        allowSkip: false,
+        timeoutMs: 60_000,
+    },
+    {
+        id: 'lite-webgpu-smoke',
+        script: 'scripts/test/lite-webgpu-smoke.ts',
+        group: 'contract',
+        packages: ['@dxo/lite'],
+        platforms: ['all'],
+        backend: ['cpu', 'webgpu'],
+        requiresGpu: false,
+        requiresNetwork: false,
+        allowSkip: false,
+        timeoutMs: 60_000,
+    },
+    {
+        id: 'runtime-contract-lite',
+        script: 'scripts/test/runtime-contract-lite.ts',
+        group: 'contract',
+        packages: ['@dxo/lite'],
+        platforms: ['all'],
+        backend: ['cpu'],
+        requiresGpu: false,
+        requiresNetwork: false,
+        allowSkip: false,
+        timeoutMs: 60_000,
+    },
+    {
+        id: 'runtime-contract-core',
+        script: 'scripts/test/runtime-contract-core.ts',
+        group: 'contract',
+        packages: ['@dxo/core'],
+        platforms: ['all'],
+        backend: ['cpu'],
+        requiresGpu: false,
+        requiresNetwork: false,
+        allowSkip: false,
+        timeoutMs: 60_000,
+    },
+    {
+        id: 'model-graph',
+        script: 'scripts/test/model-graph.ts',
+        group: 'contract',
+        packages: ['@dxo/graph', '@dxo/nn'],
+        platforms: ['all'],
+        backend: ['none'],
+        requiresGpu: false,
+        requiresNetwork: false,
+        allowSkip: false,
+        timeoutMs: 60_000,
+    },
+    {
+        id: 'profiler-trace',
+        script: 'scripts/test/profiler-trace.ts',
+        group: 'contract',
+        packages: ['@dxo/graph', '@dxo/inspect'],
+        platforms: ['all'],
+        backend: ['none'],
+        requiresGpu: false,
+        requiresNetwork: false,
+        allowSkip: false,
+        timeoutMs: 60_000,
+    },
+    {
+        id: 'titan-event-dep',
+        script: 'scripts/test/titan-event-dep.ts',
+        group: 'contract',
+        packages: ['@dxo/core'],
+        platforms: ['all'],
+        backend: ['cpu', 'cuda'],
+        requiresGpu: false,
+        requiresNetwork: false,
+        allowSkip: false,
+        timeoutMs: 60_000,
+        includeInGpuArtifact: true,
+    },
+    {
+        id: 'studio-run-smoke',
+        script: 'scripts/test/studio-run-smoke.ts',
+        group: 'product',
+        packages: ['@dxo/studio', '@dxo/inspect', '@dxo/train', '@dxo/graph', '@dxo/nn', '@dxo/data'],
+        platforms: ['all'],
+        backend: ['cpu'],
+        requiresGpu: false,
+        requiresNetwork: false,
+        allowSkip: false,
+        timeoutMs: 180_000,
+    },
+    {
+        id: 'studio-ui-wave2',
+        script: 'scripts/test/studio-ui-wave2.ts',
+        group: 'product',
+        packages: ['@dxo/studio', '@dxo/inspect'],
+        platforms: ['all'],
+        backend: ['none'],
+        requiresGpu: false,
+        requiresNetwork: false,
+        allowSkip: false,
+        timeoutMs: 60_000,
+    },
+    {
+        id: 'model-app-image',
+        script: 'scripts/test/model-app-image.ts',
+        group: 'product',
+        packages: ['@dxo/ui'],
+        platforms: ['all'],
+        backend: ['none'],
+        requiresGpu: false,
+        requiresNetwork: false,
+        allowSkip: false,
+        timeoutMs: 60_000,
+    },
+    {
+        id: 'hub-provider',
+        script: 'scripts/test/hub-provider.ts',
+        group: 'product',
+        packages: ['@dxo/hub'],
+        platforms: ['all'],
+        backend: ['none'],
+        requiresGpu: false,
+        requiresNetwork: false,
+        allowSkip: false,
+        timeoutMs: 60_000,
+    },
+    {
+        id: 'model-runtime-text',
+        script: 'scripts/test/model-runtime-text.ts',
+        group: 'product',
+        packages: ['@dxo/llm', '@dxo/nn'],
+        platforms: ['all'],
+        backend: ['cpu'],
+        requiresGpu: false,
+        requiresNetwork: false,
+        allowSkip: false,
+        timeoutMs: 180_000,
+    },
+    {
+        id: 'gpu-matmul',
+        script: 'scripts/test/gpu-matmul.ts',
+        group: 'gpu-smoke',
+        packages: ['@dxo/core'],
+        platforms: ['linux'],
+        backend: ['cuda'],
+        requiresGpu: true,
+        requiresNetwork: false,
+        allowSkip: false,
+        timeoutMs: 120_000,
+    },
+];
+
+export const SUITE_BY_ID: ReadonlyMap<string, SuiteDef> = new Map(SUITES.map((s) => [s.id, s]));
+
+export const CI_GROUPS: SuiteGroup[] = ['cpu', 'contract', 'product'];
+
+export function suiteMatchesPlatform(suite: SuiteDef, platform: NodeJS.Platform = process.platform): boolean {
+    if (suite.platforms.includes('all')) return true;
+    if (platform === 'linux' || platform === 'darwin' || platform === 'win32') {
+        return suite.platforms.includes(platform);
+    }
+    return false;
+}
+
+/** Suites run by ordinary host CI / `pnpm test:verify`. */
+export function selectCiSuites(platform: NodeJS.Platform = process.platform): SuiteDef[] {
+    return SUITES.filter(
+        (s) =>
+            CI_GROUPS.includes(s.group) &&
+            !s.requiresGpu &&
+            !s.requiresNetwork &&
+            suiteMatchesPlatform(s, platform),
+    );
+}
+
+export function selectGroupSuites(group: SuiteGroup, platform: NodeJS.Platform = process.platform): SuiteDef[] {
+    return SUITES.filter((s) => s.group === group && suiteMatchesPlatform(s, platform));
+}
+
+/** Suites packed into the Modal GPU verify artifact. */
+export function selectGpuArtifactSuites(): SuiteDef[] {
+    return SUITES.filter((s) => s.requiresGpu || s.includeInGpuArtifact === true);
+}
